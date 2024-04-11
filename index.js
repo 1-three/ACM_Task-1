d1617fea5dad2899daede37d9ba13aa6506f1fbd
aatreyee <aatreyeedev05@gmail.com>
2024-04-11 18:34:09 +0530

    updating about

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-document.getElementById("aboutbutton").classList.add("button");
-document.getElementById("aboutbutton").addEventListener("click", typeWriter);
 
-var i = 0;
-var txt = "The Association for Computing Machinery is a US-based international learned society for computing. It was founded in 1947 and is the world's largest scientific and educational computing society. The VIT Chennai Student chapter aims to foster and nurture these beliefs at the college levels";
-var speed = 50; 
+    gsap.to(".box", {duration: 2, backgroundColor: "black", onComplete: function() {
+        var text = "The Association for Computing Machinery is a US-based international learned society for computing. It was founded in 1947 and is the world's largest scientific and educational computing society. The VIT Chennai Student chapter aims to foster and nurture these beliefs at the college levels";
+        var i = 0;
+        var typingEffect = setInterval(function() {
+            document.getElementById("typing").textContent += text[i];
+            i++;
+            if (i === text.length) {
+                clearInterval(typingEffect);
+            }
+        }, 50);
+    }});
 
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("typing").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
