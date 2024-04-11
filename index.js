7d6db2ad64ccee156a2f7f281a9f371686d89501
aatreyee <aatreyeedev05@gmail.com>
2024-04-11 18:35:09 +0530

    updating about

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
     gsap.to(".box", {duration: 2, backgroundColor: "black", onComplete: function() {
         var text = "The Association for Computing Machinery is a US-based international learned society for computing. It was founded in 1947 and is the world's largest scientific and educational computing society. The VIT Chennai Student chapter aims to foster and nurture these beliefs at the college level.";
         var i = 0;
