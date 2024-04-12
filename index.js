a530a261312a2c1c1f4743d8469bb8b7ce3699db
aatreyee <145116215+1-three@users.noreply.github.com>
2024-04-12 21:49:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
     gsap.to(".box", {duration: 2, backgroundColor: "black", onComplete: function() {
-        var text = "The Association for Computing Machinery is a US-based international learned society for computing. It was founded in 1947 and is the world's largest scientific and educational computing society. The VIT Chennai Student chapter aims to foster and nurture these beliefs at the college level.";
+        var text = "The Association for Computing Machinery is a US-based international learned society for computing. It was founded in 1947 and is the world's largest scientific and educational computing society. The VIT Chennai Student Chapter aims to foster and nurture these beliefs at the college level.";
         var i = 0;
         var typingEffect = setInterval(function() {
             document.getElementById("typing").textContent += text[i];
